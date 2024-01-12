0d17e6e24a0d27c0bc2be116acd222e6556e563d
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-01-12 10:28:25 -0300

    Update Harpoon key remaps

--- lua/juampi/remap.lua
+++ lua/juampi/remap.lua
@@ -6,6 +6,7 @@ vim.keymap.set("n", "<leader>ps", function()
 end, { desc = "Project Search Word" })
 
 -- Keep buffer after pasting and replacing something
+vim.keymap.set("n", "<leader>p", '"_dP', { desc = "Paste special" })
 vim.keymap.set("n", "<leader>P", '"_dP', { desc = "Paste special" })
 
 -- Yank to clipboard
--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -5,6 +5,16 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      transparent_mode = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   -- Configure LazyVim to load catppuccin
   {
