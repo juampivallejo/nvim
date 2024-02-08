86006de20e3428dcc154866ebf44918c460ba500
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2024-02-08 19:10:25 -0300

    Add cspell as global linter

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -2,9 +2,10 @@ opts = {
   -- Event to trigger linters
   events = { "BufWritePost", "BufReadPost", "InsertLeave" },
   linters_by_ft = {
-    python = { "mypy", "cspell" },
+    python = { "mypy" },
     -- Use the "*" filetype to run linters on all filetypes.
     -- ['*'] = { 'global linter' },
+    ["*"] = { "cspell" },
     -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
     -- ['_'] = { 'fallback linter' },
   },
