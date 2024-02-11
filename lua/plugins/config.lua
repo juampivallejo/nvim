4a32f0dcb7171d90c5a9ff17945fdd60a7359bb1
juampivallejo <juampivallejo97@gmail.com>
2024-02-11 18:19:19 -0300

    Cspell with none-ls for code-actions

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -81,7 +81,8 @@ return {
 
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
-
+  -- add none-ls extra for Cspell
+  { import = "lazyvim.plugins.extras.lsp.none-ls" },
   -- add any tools you want to have installed below
   {
     "williamboman/mason.nvim",
